3a7c45923b7d76096c36f4dbcb0b73fa157dd028
ShaYri12 <sharjeelb60@gmail.com>
2025-01-25 00:12:20 +0500

    done

---
+++ app/about/page.jsx
@@ -0,0 +1,276 @@
+"use client";
+
+import { FaReact, FaNodeJs } from "@/node_modules/react-icons/fa";
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiExpress,
+  SiBootstrap,
+  SiHtml5,
+  SiCss3,
+  SiPhp,
+  SiMysql,
+  SiJavascript,
+  SiTypescript,
+  SiMongodb,
+} from "react-icons/si";
+import { motion } from "framer-motion";
+
+// components
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+// about data
+const about = {
+  title: "About me",
+  description:
+    "Driven by curiosity and perseverance, I am passionate about developing intuitive software that brings value to the world. I believe in the power of technology to bridge gaps, spark positive change, and empower users to achieve their goals.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Sharjeel",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "2+ Years",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+92) 308 2741094",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "sharjeelb60@gmail.com",
+    },
+    {
+      fieldName: "Location",
+      fieldValue: "Lyari, Karachi, Pakistan",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Urdu",
+    },
+  ],
+};
+
+// skills data
+const skills = {
+  title: "My skills",
+  description:
+    "Adept in the JavaScript ecosystem, I work with modern frameworks and tools to build fast and visually engaging web applications that deliver seamless user experiences.",
+  skillList: [
+    {
+      icon: <FaReact />,
+      name: "React.js",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Next.js",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "Node.js",
+    },
+    {
+      icon: <SiExpress />,
+      name: "Express.js",
+    },
+    {
+      icon: <SiMongodb />,
+      name: "MongoDB",
+    },
+    {
+      icon: <SiJavascript />,
+      name: "JavaScript",
+    },
+    {
+      icon: <SiTypescript />,
+      name: "TypeScript",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "TailwindCSS",
+    },
+    {
+      icon: <SiBootstrap />,
+      name: "Bootstrap",
+    },
+    {
+      icon: <SiPhp />,
+      name: "PHP",
+    },
+    {
+      icon: <SiMysql />,
+      name: "MySQL",
+    },
+    {
+      icon: <SiHtml5 />,
+      name: "HTML",
+    },
+    {
+      icon: <SiCss3 />,
+      name: "CSS",
+    },
+  ],
+};
+
+// education data
+const education = {
+  icon: "/assets/about/cap.svg",
+  title: "My education",
+  description:
+    "Solid academic foundation in computer science, enhanced by specialized certifications.",
+  items: [
+    {
+      institution: "Coursera",
+      degree: "Meta Front-End Developer Certificate",
+      duration: "2023",
+    },
+    {
+      institution: "Miami Dade College",
+      degree: "Associate Degree in Computer Science",
+      duration: "2016 - 2018",
+    },
+    {
+      institution: "Miami Dade College",
+      degree: "PHI THETA KAPPA Honor Society",
+      duration: "2015",
+    },
+    {
+      institution: "UCI (Cuba)",
+      degree: "Engineering in Computer Science",
+      duration: "2010 - 2013",
+    },
+  ],
+};
+
+const About = () => {
+  return (
+    <motion.div
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-6"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="about"
+          className="flex flex-col xl:flex-row gap-[60px] xl:px-[30px] h-full"
+        >
+          <TabsList className="flex flex-col w-full max-w-[360px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="about">About</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+          </TabsList>
+
+          {/* content */}
+          <div className="w-full min-h-[25vh]">
+            {/* about */}
+            <TabsContent value="about" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <div className="flex flex-col gap-2">
+                  <h3 className="h3">{about.title}</h3>
+                  <p className="p">{about.description}</p>
+                </div>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+                    {about.info.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-secondary h-[100px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start"
+                        >
+                          <span className="text-accent uppercase">
+                            {item.fieldName}
+                          </span>
+                          <h3 className="text-base uppercase text-white/80">
+                            {item.fieldValue}
+                          </h3>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            {/* skills */}
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <div className="flex flex-col gap-2">
+                  <h3 className="h3">{skills.title}</h3>
+                  <p className="p">{skills.description}</p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-secondary rounded-xl flex flex-col justify-center items-center group">
+                              <div className="text-5xl group-hover:text-accent transition-all duration-300">
+                                {skill.icon}
+                              </div>
+                              {/* Display the skill name directly on smaller screens */}
+                              <p className="text-sm mt-2 xl:hidden group-hover:text-accent transition-all duration-300">
+                                {skill.name}
+                              </p>
+                            </TooltipTrigger>
+                            {/* Tooltip content only visible on xl screens and larger */}
+                            <TooltipContent className="hidden xl:block">
+                              <p>{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+
+            {/* education */}
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <div className="flex flex-col gap-2">
+                  <h3 className="h3">{education.title}</h3>
+                  <p className="p">{education.description}</p>
+                </div>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-secondary h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="max-w-[260px] min-h-[60px] flex items-center justify-center">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            {/* dot */}
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.institution}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
+};
+
+export default About;
---
+++ app/projects/page.jsx
@@ -0,0 +1,160 @@
+"use client";
+
+import React from "react";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import Link from "next/link";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const projects = [
+  {
+    num: "01",
+    category: "fullstack",
+    title: "Product Store App",
+    description: "A dynamic full-stack product management application.",
+    stack: [{ name: "React.js" }, { name: "Node.js" }, { name: "MongoDB" }],
+    image: "/assets/projects/thumb1.png",
+    live: "https://product-store-app-tkmt.onrender.com/",
+    github: "https://github.com/solarluiso/product-store-app",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "Mood Mapper",
+    description:
+      "A web application to track and analyze your mood throughout the days.",
+    stack: [{ name: "Next.js" }, { name: "TailwindCSS" }, { name: "Firebase" }],
+    image: "/assets/projects/thumb2.png",
+    live: "https://mood-mapper.netlify.app/",
+    github: "https://github.com/solarluiso/mood-tracker",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "E-commerce Store",
+    description:
+      "A responsive e-commerce store to browse, view, and favorite products.",
+    stack: [{ name: "React.js" }, { name: "TailwindCSS" }, { name: "Vite" }],
+    image: "/assets/projects/thumb3.png",
+    live: "https://ecommerce-ten-alpha-35.vercel.app/",
+    github: "https://github.com/solarluiso/ecommerce",
+  },
+  {
+    num: "04",
+    category: "frontend",
+    title: "Focus",
+    description:
+      "A sleek web application designed to enhance productivity and focus.",
+    stack: [
+      { name: "Next.js" },
+      { name: "TailwindCSS" },
+      { name: "Framer-Motion" },
+    ],
+    image: "/assets/projects/thumb4.png",
+    live: "https://focus-inky.vercel.app/",
+    github: "https://github.com/solarluiso/focus",
+  },
+  {
+    num: "05",
+    category: "frontend",
+    title: "Fit Physique",
+    description: "A fitness-focused platform to track workouts and progress.",
+    stack: [
+      { name: "Next.js" },
+      { name: "TypeScript" },
+      { name: "TailwindCSS" },
+    ],
+    image: "/assets/projects/thumb5.png",
+    live: "https://fitphysique-pi.vercel.app/",
+    github: "https://github.com/solarluiso/fitphysique",
+  },
+  {
+    num: "06",
+    category: "frontend",
+    title: "Modern Bank App",
+    description:
+      "An innovative banking application with a modern user interface.",
+    stack: [{ name: "React.js" }, { name: "TailwindCSS" }, { name: "Vite" }],
+    image: "/assets/projects/thumb6.png",
+    live: "https://modern-bank-app-ebon-omega.vercel.app/",
+    github: "https://github.com/solarluiso/modern-bank-app",
+  },
+];
+
+const Projects = () => {
+  return (
+    <motion.div
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-6"
+    >
+      <div className="container mx-auto">
+        <h3 className="h3 mb-8 text-accent text-center">My Projects</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px]">
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              className="relative group rounded-xl overflow-hidden shadow-lg"
+            >
+              {/* Image */}
+              <div className="relative w-full h-60">
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  fill
+                  quality={90}
+                  sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 400px"
+                  className="object-cover rounded-xl"
+                  priority={project.num === "02"}
+                />
+              </div>
+
+              {/* Project Content */}
+              <div className="absolute bottom-0 left-0 right-0 p-4 bg-black/95 text-white rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <h3 className="font-bold text-lg">{project.title}</h3>
+                <p className="text-sm mt-2 text-white/80">
+                  {project.description}
+                </p>
+
+                {/* Stack */}
+                <ul className="flex gap-2 text-xs mt-2">
+                  {project.stack.map((item, index) => (
+                    <li key={index} className="text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  ))}
+                </ul>
+
+                {/* Buttons */}
+                <div className="mt-4 flex gap-4">
+                  <Link
+                    href={project.live}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="w-[40px] h-[40px] rounded-full bg-white/5 flex justify-center items-center group">
+                      <BsArrowUpRight className="text-white text-xl group-hover:text-accent" />
+                    </div>
+                  </Link>
+                  <Link
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="w-[40px] h-[40px] rounded-full bg-white/5 flex justify-center items-center group">
+                      <BsGithub className="text-white text-xl group-hover:text-accent" />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Projects;
